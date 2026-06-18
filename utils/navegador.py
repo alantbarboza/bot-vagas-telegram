48bbdde693e7d91e5d237ef46f073c1e8c43963f
Alan <67348179+alantbarboza@users.noreply.github.com>
2026-06-18 19:00:46 -0300

    alterei o navegador.py para funcionar no render

--- utils/navegador.py
+++ utils/navegador.py
@@ -10,17 +10,12 @@ def criar_navegador_humano(playwright):
     )
 
     context = browser.new_context(
-        viewport={
-            "width": 1366,
-            "height": 768
-        },
-
+        viewport={"width": 1366, "height": 768},
         user_agent=(
             "Mozilla/5.0 (Windows NT 10.0; Win64; x64) "
             "AppleWebKit/537.36 (KHTML, like Gecko) "
             "Chrome/136.0.0.0 Safari/537.36"
         ),
-
         locale="pt-BR",
         timezone_id="America/Sao_Paulo",
         color_scheme="light"
@@ -29,9 +24,9 @@ def criar_navegador_humano(playwright):
     page = context.new_page()
 
     page.add_init_script("""
-    Object.defineProperty(navigator, 'webdriver', {
-        get: () => undefined
-    })
+        Object.defineProperty(navigator, 'webdriver', {
+            get: () => undefined
+        })
     """)
 
     page.set_extra_http_headers({
@@ -39,7 +34,9 @@ def criar_navegador_humano(playwright):
         "Upgrade-Insecure-Requests": "1"
     })
 
+    page.set_default_timeout(60000)
+    page.set_default_navigation_timeout(60000)
+
     page.mouse.move(500, 300)
-    page.wait_for_timeout(800)
 
     return browser, page
